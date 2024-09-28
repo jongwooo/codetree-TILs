e5d8b734e223c53bdb6c186b5228fa56509391e7
Jongwoo Han <jongwooo.han@gmail.com>
2024-09-28 22:37:54 +0900

    Title: 불안한 무빙워크, Time: 409ms, Memory: 27MB, Status: Passed - CodeTree

--- 240928/불안한 무빙워크/unstable-moving-walk.py
+++ 240928/불안한 무빙워크/unstable-moving-walk.py
@@ -6,14 +6,20 @@ def test_moving_work():
     # 무빙워크가 한 칸 회전한다
     moving_work.appendleft(moving_work.pop())
     person.appendleft(person.pop())
+    # n번 칸에 사람이 있으면 내린다
     if person[n - 1]:
         person[n - 1] = 0
+    # 무빙워크가 회전하는 방향으로 한 칸 이동할 수 있으면 이동한다
     for i in range(n - 1, 0, -1):
         if person[i - 1] and not person[i] and moving_work[i] > 0:
             person[i - 1], person[i] = person[i], person[i - 1]
             moving_work[i] -= 1
             if moving_work[i] == 0:
                 durability_zero_cnt += 1
+    # n번 칸에 사람이 있으면 내린다
+    if person[n - 1]:
+        person[n - 1] = 0
+    # 1번 칸에 사람이 없고 안정성이 0이 아니라면 사람을 한 명 더 올린다
     if not person[0] and moving_work[0] > 0:
         person[0] = 1
         moving_work[0] -= 1
