99504c783f958efd422c2120e87e1cbaed2ceb3c
Jongwoo Han <jongwooo.han@gmail.com>
2024-10-08 00:26:04 +0900

    Title: 자율주행 전기차, Time: 342ms, Memory: 26MB, Status: Passed - CodeTree

--- 241008/자율주행 전기차/autonomous-electric-car.py
+++ 241008/자율주행 전기차/autonomous-electric-car.py
@@ -23,6 +23,8 @@ def find_nearest_passenger():
             if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == -1 and road[nx][ny] != ROCK:
                 queue.append((nx, ny))
                 visited[nx][ny] = visited[x][y] + 1
+    if not candidates:
+        return -1, -1, -1
     candidates.sort()
     return candidates[0]
 
@@ -33,7 +35,7 @@ def find_passenger_destination(pid):
     visited[ex][ey] = 0
     while queue:
         x, y = queue.popleft()
-        if destination_pos[x][y] == pid:
+        if pid in destination_pos[x][y]:
             return visited[x][y], x, y
         for dx, dy in dirs:
             nx = x + dx
@@ -41,22 +43,25 @@ def find_passenger_destination(pid):
             if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == -1 and road[nx][ny] != ROCK:
                 queue.append((nx, ny))
                 visited[nx][ny] = visited[x][y] + 1
+    return -1, -1, -1
 
 
 dirs = ((-1, 0), (0, 1), (1, 0), (0, -1))
 n, m, c = map(int, input().split())
 road = [list(map(int, input().split())) for _ in range(n)]
 passenger_pos = [[0] * n for _ in range(n)]
-destination_pos = [[0] * n for _ in range(n)]
+destination_pos = [[[] for _ in range(n)] for _ in range(n)]
 ex, ey = map(int_minus_one(), input().split())
 for pid in range(2, m + 2):
     x_s, y_s, x_e, y_e = map(int_minus_one(), input().split())
     passenger_pos[x_s][y_s] = pid
-    destination_pos[x_e][y_e] = pid
+    destination_pos[x_e][y_e].append(pid)
 passenger_cnt = m
 while passenger_cnt > 0:
     # 승객을 태우러 출발지에 이동
     dist, tx, ty = find_nearest_passenger()
+    if dist == tx == ty == -1:
+        break
     if c < dist:
         break
     c -= dist
@@ -65,10 +70,12 @@ while passenger_cnt > 0:
     ex, ey = tx, ty
     # 승객을 태우고 목적지로 이동
     dist, tx, ty = find_passenger_destination(pid)
+    if dist == tx == ty == -1:
+        break
     if c < dist:
         break
     c += dist
-    destination_pos[tx][ty] = EMPTY
+    destination_pos[tx][ty].remove(pid)
     ex, ey = tx, ty
     passenger_cnt -= 1
 print(c if not passenger_cnt else -1)
